08a1ecb207aa1cae7c81b96e8455b80137929d67
Soukayna El Ferchouni <soukaynafr2019@gmail.com>
2023-09-08 17:20:24 +0100

    integrating the OpenWeather API

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -7,8 +7,9 @@ import { response } from 'express';
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit{
+  
 WeatherData:any;
-cityName?:string;
+cityName:string='London';
 constructor(){}
 
 
@@ -17,17 +18,20 @@ constructor(){}
       main: {}
     }
     this.getWeatherData();
-    console.log(this.WeatherData);
+    this.cityName ='';
   }
   
+  onSubmit(){
+    this.getWeatherData();
+    this.cityName ='';
+  }
 
   getWeatherData(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=15157990a839b26e8e8c626470c01af8')
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.cityName}&appid=15157990a839b26e8e8c626470c01af8`)
+  
     .then(response => response.json())
     .then(data =>{this.setWeatherData(data);})
 
-  //   let data = JSON.parse('{"coord":{"lon":10.99,"lat":44.34},"weather":[{"id":800,"main":"Clear","description":"clear sky","icon":"01d"}],"base":"stations","main":{"temp":298.82,"feels_like":298.62,"temp_min":296.86,"temp_max":300.05,"pressure":1018,"humidity":45,"sea_level":1018,"grnd_level":936},"visibility":10000,"wind":{"speed":3.01,"deg":30,"gust":3.59},"clouds":{"all":0},"dt":1694183105,"sys":{"type":2,"id":2075663,"country":"IT","sunrise":1694148391,"sunset":1694194899},"timezone":7200,"id":3163858,"name":"Zocca","cod":200}')
-  // this.setWeatherData(data);
   }
 
   setWeatherData(data: any){
